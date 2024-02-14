e34265b67971179f2e2d36904b9e0b5c7d8bed3e
Łukasz Kurdziel <lukasz.kurdziel99@gmail.com>
2024-02-14 15:51:10 +0100

    add dist

--- src/app/common/resourceHandler.ts
+++
@@ -1,7 +0,0 @@
-export class ResourceHandler {
-  private static readonly ASSETS_PATH = '/Space-ShootOut/src/assets/';
-
-  public static getResourcePath(filename: string): string {
-    return this.ASSETS_PATH + filename;
-  }
-}
--- vite.config.ts
+++
@@ -1,5 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  base: 'https://luukiii.github.io/Space-ShootOut/',
-});
